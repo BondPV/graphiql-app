1727337dbde31bf85e1e78df7b94e24600366573
BondPV <bondpv@yandex.ru>
2023-05-02 13:42:30 +0300

    feat: add material-ui

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+export enum Lang {
+  en = 'en',
+  ru = 'ru',
+}
 
 const LanguageSwitcher = (): JSX.Element => {
   const { i18n } = useTranslation();
 
-  const changeLanguage = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-    i18n.changeLanguage(event.target.value);
+  const changeLanguage = (event: React.MouseEvent<HTMLElement>, newLanguage: string): void => {
+    i18n.changeLanguage(newLanguage);
   };
 
   return (
-    <select onChange={changeLanguage} value={i18n.language}>
-      <option value="en">English</option>
-      <option value="ru">Русский</option>
-    </select>
+    <ToggleButtonGroup value={i18n.language} exclusive onChange={changeLanguage}>
+      <ToggleButton value={Lang.ru}>RU</ToggleButton>
+      <ToggleButton value={Lang.en}>EN</ToggleButton>
+    </ToggleButtonGroup>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import theme from './styles/theme';
 import './i18n';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
