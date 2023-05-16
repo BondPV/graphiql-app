aa4839e4be40635bc17d000b44e6ade7d07e751c
Alesia-V175 <vaivasilisa@gmail.com>
2023-05-16 17:23:10 +0300

    feat: update welcome page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Container } from '@mui/material';
+import { Box } from '@mui/material';
+import backgroundImage from '@/assets/bg-welcome.svg';
 import { AuthContext } from '@/App/App';
 import { ROUTE } from '@/constants';
 
@@ -19,11 +21,23 @@ const WelcomePage = (): JSX.Element => {
   };
 
   return (
-    <Container maxWidth="xl">
-      <div>
+    <Container
+      maxWidth="xl"
+      sx={{
+        background: 'linear-gradient(222.57deg, #FFFFFF -5.18%, #EBEEF3 24.79%, #DADEEA 71.54%)',
+      }}
+    >
+      <Box
+        sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: '55% -33rem',
+          minHeight: '100rem',
+        }}
+      >
         <p>{t('welcomePage')}</p>
         <button onClick={redirect}>{t('startButton')}</button>
-      </div>
+      </Box>
     </Container>
   );
 };
