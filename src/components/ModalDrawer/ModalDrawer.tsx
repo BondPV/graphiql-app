df4760717d5a24d5a2c64488494cd3f3cffd3e74
BondPV <bondpv@yandex.ru>
2023-05-17 16:17:29 +0300

    feat: add close button to modul drawer

--- src/components/ModalDrawer/ModalDrawer.tsx
+++ src/components/ModalDrawer/ModalDrawer.tsx
@@ -1,4 +1,5 @@
-import { Box, Modal } from '@mui/material';
+import { Close } from '@mui/icons-material';
+import { Box, IconButton, Modal } from '@mui/material';
 
 interface IModalDrawerProps {
   children: React.ReactNode;
@@ -8,26 +9,29 @@ interface IModalDrawerProps {
 
 const ModalDrawer = ({ children, openDrawer, toggleDrawer }: IModalDrawerProps): JSX.Element => {
   return (
-    <div>
-      <Modal open={openDrawer} onClose={toggleDrawer} sx={{ display: { xs: 'block', md: 'none' } }}>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: { xs: '95%', sm: '70%' },
-            height: '80vh',
-            overflow: 'auto',
-            bgcolor: 'background.paper',
-            boxShadow: 24,
-            p: 2,
-          }}
-        >
-          {children}
+    <Modal open={openDrawer} onClose={toggleDrawer} sx={{ display: { xs: 'block', md: 'none' } }}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: { xs: '95%', sm: '70%' },
+          height: '80vh',
+          overflow: 'auto',
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 2,
+        }}
+      >
+        <Box position={'absolute'} top={5} right={5} zIndex={1300}>
+          <IconButton aria-label="close" onClick={toggleDrawer}>
+            <Close />
+          </IconButton>
         </Box>
-      </Modal>
-    </div>
+        {children}
+      </Box>
+    </Modal>
   );
 };
 
